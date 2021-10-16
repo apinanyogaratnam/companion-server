82a4a0ecdd6f6a28db2150fb35939e4abd794903
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-15 21:28:16 -0400

    basic mondodb code

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def index():
 
 
 @app.route('/api/v1/')
+def get_all_data():
+    client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.zodle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client.test
 
 if __name__ == '__main__':
     app.run(debug=False)

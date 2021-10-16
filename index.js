034f89fa950c5a3797db12f977c524eae256fb47
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-16 11:49:47 -0400

    commented dir logs

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const Person = mongoose.model('Person', schema);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => { console.dir(req); next(); });
+// app.use((req, res, next) => { console.dir(req); next(); });
 
 app.get("/", function(req, res) {
     res.json({greeting: "Welcome to main the API of companion"});

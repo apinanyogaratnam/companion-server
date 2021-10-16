03842e949b66819fc08146a7c076207d4d4da41e
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-15 21:42:01 -0400

    created template code to start with api requests

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+var express = reqire('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
+
+const port = process.env.PORT || 3000;
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(cors());
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.json({"Welcome to main the API of companion"});
+});
+
+var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + port);
+});

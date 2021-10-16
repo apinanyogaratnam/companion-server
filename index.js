dbfc37abf0986436a2313e1357242903ad4d4b9a
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-15 22:13:39 -0400

    created schema and post and get req for users

--- index.js
+++ index.js
@@ -8,11 +8,37 @@ var cors = require('cors');
 const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const schema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    age: Number,
+    logs: [
+        {
+            date: Date,
+            message: String,
+            mood: String
+        }
+    ]
+});
+
+const Person = mongoose.model('Person', schema);
+
 app.use(cors());
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
     res.json({greeting: "Welcome to main the API of companion"});
+});
+
+app.post("/api/v1/users", function(req, res) {
+    var user = req.body;
+    console.log(user);
+});
+
+app.get("/api/v1/users", function(req, res) {
+    // get all the users from the mongodb
+    // return the users in json format
 });
 
 var listener = app.listen(port, function () {

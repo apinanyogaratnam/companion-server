28d71b790378412074ed35eed5ee3281fbd03c63
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-15 21:52:40 -0400

    created home page greeting json data

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 
 const port = process.env.PORT || 3000;
+
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors());
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-    res.json({"Welcome to main the API of companion"});
+    res.json({greeting: "Welcome to main the API of companion"});
 });
 
 var listener = app.listen(process.env.PORT, function () {
